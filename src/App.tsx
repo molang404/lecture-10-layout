87a1206a661f62e3b343cd5c0d252de45a1527e7
molang404 <art104233@naver.com>
2026-04-27 14:24:15 +0900

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes } from "react-router";
+
 function App() {
-  return <div></div>;
+  return (
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
