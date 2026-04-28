5c4e90ba28a7c7121357c98b90090b4aefc521aa
molang404 <art104233@naver.com>
2026-04-28 15:39:42 +0900

    localStorage 연동 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import GlobalStyle from "./styles/GlobalStyle.ts";
 import { AppRouter } from "./router/AppRouter.tsx";
 import { ThemeProvider } from "styled-components";
 import { DarkTheme, LightTheme } from "./styles/theme.ts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [theme, setTheme] = useState<"light" | "dark">("light"); // 테마 상태를 저장하는 state
+    // 초기값 자리에 함수를 집어넣을 수도 있음
+    // useState가 state를 만들 때 그 함수를 실행시키고 난 결과를 초기값으로 사용함
+    const [theme, setTheme] = useState<"light" | "dark">(() => {
+        // localStorage에서 가져오는 값은, 무조건 string | null
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme === "dark" ? "dark" : "light";  // "dark"가 아니든, 없든 무조건 "light"
+    }); // 테마 상태를 저장하는 state
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    }, [theme]);
 
     const onClick = () => {
-        if (theme === "light") {
-            setTheme("dark");
-        } else {
-            setTheme("light");
-        }
+        // setState에도 매개변수로 함수를 작성할 수 있는데,
+        // 이 때 함수의 매개변수 자리에는 "지금 현재 state 값"이 들어옴
+        setTheme((prev) => prev === "dark" ? "light" : "dark");
     };
 
     return (
